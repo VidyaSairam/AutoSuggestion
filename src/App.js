27d241476eb4c9f79d1a1f62605d1e657e699fd4
sai <bvidyasairam@yahoo.com>
2021-10-14 23:28:46 +0530

    auto suggestion for new component works

--- src/App.js
+++ src/App.js
@@ -1,23 +1,112 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  // const [text, setText] = useState([
+  //   {
+  //     source_component: '', // there is only 1 parent component
+  //     operator: '',
+  //     target_component: '',
+  //     enable: false
+  //   }
+  // ])
+
+  const [text, setText] = useState([
+    {
+      component_or_operator: '',
+      isOperator: false,
+      disabled: false
+    }
+  ])
+
+  const [suggestion, setSuggestion] = useState([])
+
+  // const handleSuggestionChange = (e) => {
+  //   console.log(e.target.value)
+  //   let existing_content = text
+  //   const val = {
+  //     name: e.target.value
+  //   }
+  //   existing_content.push(val)
+  //   setText(existing_content)
+  // }
+
+  const handleSearchChange = (e) => {
+    const val = e.target.value
+    let response
+    if (val == '+' || val == '-' || val == '*' || val == '/') {
+      response = ['+', '-', '*', '/'].filter(item => item.includes(val))
+    }
+    else {
+      response = search.filter(item => item.toLowerCase().includes(e.target.value))
+    }
+    setSuggestion(response)
+    
+    let arrContent = text
+    console.log(parseInt(arrContent.length)-1)
+    arrContent[arrContent.length-1].component_or_operator = val;
+    setTimeout(()=>{
+      setText(arrContent)
+    },50)
+    
+
+  }
+  const handleSuggestionChange = (e,item) => {
+    let res = text.slice(0,text.length-1)
+    const val = e.target.value
+    const lastCont =  {
+      component_or_operator: '',
+      isOperator: false,
+      disabled: false
+    }
+    if (val == '+' || val == '-' || val == '*' || val == '/') {
+      const content = {
+        component_or_operator: item,
+        isOperator: true,
+        disabled: false
+      }
+      res.push(content)
+    }
+    else{
+      const content = {
+        component_or_operator: item,
+        isOperator: false,
+        disabled: false
+      }
+      res.push(content)
+    }
+    res.push(lastCont)
+    console.log(res)
+    setText([])
+    setTimeout(()=>{
+      setText(res)
+    },500)
+
+  }
+  const search = ['basic', 'hra', 'allowance']
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className={'container'}>
+        {
+          text.map((item, index) => (
+            <input disabled={item.disabled} value={item.component_or_operator} onChange={handleSearchChange} />
+          ))
+        }
+
+        <div>{
+          suggestion ?
+            suggestion.map((item) => (
+              <div>
+                <p onClick={(e) => handleSuggestionChange(e,item)}>{item}</p>
+              </div>
+            ))
+            : null
+        }
+        </div>
+      </div>
+
     </div>
   );
 }
