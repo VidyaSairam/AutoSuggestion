ce6a0d926eeb81cfa1928e6d4868b2e8595fb05b
sai <bvidyasairam@yahoo.com>
2021-10-19 21:01:20 +0530

    mutliple features such as add, shift, remove and subtypes of it are added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 function App() {
 
-  const [text, setText] = useState([
-    {
-      component_or_operator: '',
-      isOperator: false,
-      disabled: false,
-      color: 'black',
-      isParanthesis : false,
-      componentType:''
-    }
+  const [text, setText] = useState([{
+    component_or_operator: '(',
+    isOperator: false,
+    disabled: true,
+    color: 'black',
+    isParanthesis: true,
+    isClosingParanthesis: false,
+    componentType: 'paranthesis'
+  },
+  {
+    component_or_operator: '',
+    isOperator: false,
+    disabled: false,
+    color: 'black',
+    isParanthesis: false,
+    isClosingParanthesis: false,
+    componentType: ''
+  },
+  {
+    component_or_operator: ')',
+    isOperator: false,
+    disabled: true,
+    color: 'black',
+    isParanthesis: true,
+    isClosingParanthesis: true,
+    componentType: 'paranthesis'
+  }
   ])
 
   const [finalFormula, setFinalFormula] = useState([])
@@ -23,32 +41,32 @@ function App() {
 
   const [index, setIndex] = useState(0)
   const [suggestion, setSuggestion] = useState([])
-  const operator = ['+', '-', '*', '/','(',')']
+  const operator = ['+', '-', '*', '/', '(']
 
   //search
-  const handleSearchChange = (e, index,item) => {
+  const handleSearchChange = (e, index, item) => {
     const val = e.target.value
     let response
-    const operatorCond = (val == '+' || val == '-' || val == '*' || val == '/' || val == '(' || val == ')') ? true : false
+    const operatorCond = (val == '+' || val == '-' || val == '*' || val == '/' || val == '(') ? true : false
     const numberCond = !isNaN(val)
-    
+
     if (operatorCond) {
       response = operator.filter(item => item.includes(val))
     }
-    else if(numberCond){
-      
+    else if (numberCond) {
+
       let num = []
       num.push(val)
       response = num.filter(item => item.includes(val))
     }
-    else if(val == ''){
+    else if (val == '') {
       response = []
     }
-    else{
+    else {
       response = search.filter(item => item.toLowerCase().includes(e.target.value))
     }
     setSuggestion(response)
-  
+
     let arrContent = text
     arrContent[index].component_or_operator = val
     setTimeout(() => {
@@ -59,34 +77,86 @@ function App() {
 
 
   const handleSuggestionChange = (e, item, parentIndex = null) => {
-   if(item==0){
+    if (item == 0) {
       alert('0 cannot be used')
       return
-    } 
-    let res = text.slice(0, text.length - 1)
+    }
+    let res = (parentIndex == text.length - 2) ? text.slice(0, text.length - 1) : text
+    const isParentIndex = parentIndex == res.length - 1
     const lastCont = {
       component_or_operator: '',
       isOperator: false,
       disabled: false,
       color: 'black',
-      isParanthesis : false,
-      componentType:''
+      isParanthesis: false,
+      isClosingParanthesis: false,
+      componentType: ''
     }
     if (operator.includes(item)) {
-      const content = {
-        component_or_operator: item,
-        isOperator: true,
-        disabled: false,
-        color: 'red',
-        componentType : 'operator',
-        isParanthesis : false
-      }
-      // res.push(content)
-      if (parentIndex != null) {
+      if (item == "(") {
+        const content = {
+          component_or_operator: item,
+          isOperator: false,
+          disabled: false,
+          color: 'green',
+          componentType: 'paranthesis',
+          isParanthesis: true,
+          isClosingParanthesis: false,
+        }
+        console.log("parent index ", parentIndex)
         res[parentIndex] = content
+        res.splice(parentIndex + 1, 0, {
+          component_or_operator: "",
+          isOperator: false,
+          disabled: false,
+          color: 'black',
+          componentType: '',
+          isParanthesis: false,
+          isClosingParanthesis: false
+        })
+        res.splice(parentIndex + 2, 0, {
+          component_or_operator: ")",
+          isOperator: false,
+          disabled: false,
+          color: 'green',
+          componentType: 'paranthesis',
+          isParanthesis: true,
+          isClosingParanthesis: true
+        })
+        // if (parentIndex != null) {
+        //   res[parentIndex] = content
+        // }
+        // else {
+        //   res.push(content)
+        //   res.push({
+        //     component_or_operator: ")",
+        //     isOperator: false,
+        //     disabled: false,
+        //     color: 'green',
+        //     componentType: 'paranthesis',
+        //     isParanthesis: true,
+        //     isClosingParanthesis: true,
+        //   })
+        // }
       }
       else {
-        res.push(content)
+        const content = {
+          component_or_operator: item,
+          isOperator: true,
+          disabled: false,
+          color: 'red',
+          componentType: 'operator',
+          isParanthesis: false,
+          isClosingParanthesis: false,
+        }
+        // res.push(content)
+        // if (parentIndex != null) {
+        //   res[parentIndex] = content
+        // }
+        // else {
+        //   res.push(content)
+        // }
+        res[parentIndex] = content
       }
     }
     else {
@@ -95,11 +165,10 @@ function App() {
         isOperator: false,
         disabled: false,
         color: 'green',
-        componentType : isSource? "source" : "target",
-        isParanthesis : false
+        componentType: isSource ? "source" : "target",
+        isParanthesis: false,
+        isClosingParanthesis: false,
       }
-      // res.push(content)
-      // res[parentIndex] = content 
       if (parentIndex != null) {
         res[parentIndex] = content
       }
@@ -107,14 +176,25 @@ function App() {
         res.push(content)
       }
     }
-    
-    if(res[res.length-1].componentType=="target")
-      res = handleWithParams(res)
-    
-    
-    res.push(lastCont)
+
+    // if (res[res.length - 1].componentType == "target")
+    //   res = handleWithParams(res)
+
+    if (isParentIndex) {
+      res.push(lastCont)
+      res.push({
+        component_or_operator: ')',
+        isOperator: false,
+        disabled: true,
+        color: 'black',
+        isParanthesis: true,
+        isClosingParanthesis: true,
+        componentType: 'paranthesis'
+      })
+    }
     setIsSource(false)
-    console.log(res)
+
+
     //handle for search
     setText([])
     setSuggestion([])
@@ -122,8 +202,8 @@ function App() {
     setTimeout(() => {
       setText(res)
     }, 500)
-    console.log(res)
   }
+
   const search = ['basic', 'hra', 'allowance']
 
   function handleWithParams(res) {
@@ -134,40 +214,217 @@ function App() {
       isOperator: false,
       disabled: true,
       color: 'black',
-      isParanthesis : true,
-      componentType:'paranthesis'
+      isParanthesis: true,
+      isClosingParanthesis: false,
+      componentType: 'paranthesis'
     }
 
-    if(arg[0].componentType == "source" || arg[0].isParanthesis ){
+    if (arg[0].componentType == "source" || arg[0].isParanthesis) {
       arg.unshift(params)
     }
-    
+
     params = {
       component_or_operator: ')',
       isOperator: false,
       disabled: true,
       color: 'black',
-      isParanthesis : true,
-      componentType:'paranthesis'
+      isParanthesis: true,
+      isClosingParanthesis: true,
+      componentType: 'paranthesis'
     }
     arg.push(params)
     return arg
   }
 
 
+  Array.prototype.insert = function (index, item) {
+    this.splice(index, 0, item);
+  }
 
-  
+  const handleLeftShift = (index) => {
+    let res = text
+    if (index == 0 || index == 1 || index == text.length - 1) {
+      alert("cannot left shift starting element")
+      return;
+    }
+
+    [res[index - 1], res[index]] = [res[index], res[index - 1]]
+    setText([])
+    setTimeout(() => {
+      setText(res)
+    }, 50)
+
+
+  }
+
+  const handleRightShift = (index) => {
+
+    let res = text
+    if (index == text.length - 1) {
+      alert("cannot shift ending element")
+      return;
+    }
+
+    [res[index], res[index + 1]] = [res[index + 1], res[index]]
+    setText([])
+    setTimeout(() => {
+      setText(res)
+    }, 50)
+
+
+
+  }
+
+  const handleAddParamsLeft = (index) => {
+    let res = text
+    if (index == 0) {
+      res.unshift({
+        component_or_operator: '',
+        isOperator: false,
+        disabled: false,
+        color: 'black',
+        isParanthesis: false,
+        isClosingParanthesis: false,
+        componentType: ''
+      })
+    }
+    else {
+      res.insert(index,
+        {
+          isOperator: false,
+          component_or_operator: '',
+          disabled: false,
+          color: 'black',
+          isParanthesis: false,
+          isClosingParanthesis: false,
+          componentType: ''
+        })
+    }
+    setText([])
+    setTimeout(() => {
+      setText(res)
+    }, 50)
+
+  }
+
+  const handleAddParamsRight = (index) => {
+    let res = text
+    res.insert(index + 1, {
+      component_or_operator: '',
+      isOperator: false,
+      disabled: false,
+      color: 'black',
+      isParanthesis: false,
+      isClosingParanthesis: false,
+      componentType: ''
+    })
+
+    setText([])
+    setTimeout(() => {
+      setText(res)
+    }, 50)
+  }
+
+  // const handleAddParams = (index) => {
+  //   // console.log(index)
+  //   const res = text;
+  //   res.insert(index + 1, {
+  //     component_or_operator: '',
+  //     isOperator: false,
+  //     disabled: false,
+  //     color: 'black',
+  //     isParanthesis: false,
+  //     isClosingParanthesis: false,
+  //     componentType: ''
+  //   })
+  //   res.insert(index + 2, {
+  //     component_or_operator: '',
+  //     isOperator: false,
+  //     disabled: false,
+  //     color: 'black',
+  //     isParanthesis: false,
+  //     isClosingParanthesis: false,
+  //     componentType: ''
+  //   })
+  //   let params = {
+  //     component_or_operator: ')',
+  //     isOperator: false,
+  //     disabled: true,
+  //     color: 'black',
+  //     isParanthesis: true,
+  //     isClosingParanthesis: false,
+  //     componentType: 'paranthesis'
+  //   }
+  //   res.insert(index + 3, params)
+  //   params = {
+  //     component_or_operator: '(',
+  //     isOperator: false,
+  //     disabled: true,
+  //     color: 'black',
+  //     isParanthesis: true,
+  //     isClosingParanthesis: false,
+  //     componentType: 'paranthesis'
+  //   }
+  //   res.unshift(params)
+  //   console.log(res)
+  //   setText([])
+  //   setTimeout(() => {
+  //     setText(res)
+  //   }, 50)
+
+
+  // }
+
+  const handleDeleteParams = (index) => {
+    console.log(index, text, index + 1)
+    // console.log("handle delete params")
+    let res = text
+    const startingIndex = index
+    res.splice(startingIndex, 1)
+    console.log(res)
+    setText([])
+    setTimeout(() => {
+      setText(res)
+    }, 50)
+  }
+
   return (
     <div>
       <header>Proof of concept - auto suggestion</header>
       <div className={'container'}>
+
+
         {
-          text.map((item, parentIndex) => (
-            <input disabled={item.disabled} style={{ color: item.color }} value={item.component_or_operator} onChange={(e) => {
-              setIndex(parentIndex)
-              handleSearchChange(e, parentIndex,item)
-            }} />
-          ))
+
+          text.map((item, parentIndex) => {
+            {/* console.log(item, "textItem") */ }
+            return (
+              <div className="input_row_container">
+                <div className="input_button_container">
+                  <div>
+                    <input className={"input_"} disabled={item.disabled} style={{ color: item.color }} value={item.component_or_operator} onChange={(e) => {
+                      setIndex(parentIndex)
+                      handleSearchChange(e, parentIndex, item)
+                    }} />
+                  </div>
+                  {/* <div hidden={item.isClosingParanthesis} className={'hide'} > */}
+                  <div className={'hide'}>
+                    <div id="buttons_container">
+                      <div id="button_container">
+                        <button onClick={() => handleAddParamsLeft(parentIndex)} id="button1">{"+L"}</button>
+                        <button onClick={() => handleAddParamsRight(parentIndex)} id="button1">{"+R"}</button>
+                        <button onClick={() => handleDeleteParams(parentIndex)} id="button2" >{"X"}</button>
+                      </div>
+                      <div id="button_container">
+                        <button onClick={() => handleLeftShift(parentIndex)} id="button3">{"<"}</button>
+                        <button onClick={() => handleRightShift(parentIndex)} id="button3">{">"}</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            )
+          })
         }
         <div>
           {
@@ -201,9 +458,6 @@ function App() {
           }
         </select>
       </div>
-      {/* <div>
-        <input type = "submit" onClick={() => console.log(finalFormula)} />
-      </div> */}
     </div>
   );
 }
